0248af97e8b1370254e975a9756b7ebbe14a1c8e
Dawson <hello@dawson.gg>
2023-10-18 20:04:05 -0400

    feat: Maven Publishing, added semver versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "1.9.0"
+    id("maven-publish")
 }
 
 group = "gg.flyte"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -19,4 +20,33 @@ tasks.test {
 
 kotlin {
     jvmToolchain(17)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "flyte-repository"
+            url = uri(
+                "https://repo.flyte.gg/${
+                    if (version.toString().endsWith("-SNAPSHOT")) "snapshots" else "releases"
+                }"
+            )
+            credentials {
+                username = System.getenv("MAVEN_NAME")
+                password = System.getenv("MAVEN_SECRET")
+            }
+        }
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = group.toString()
+                artifactId = "discordgenerator"
+                version = version.toString()
+
+                from(components["java"])
+            }
+        }
+    }
 }
